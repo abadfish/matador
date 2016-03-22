c026e61aa1330b7d58225e3a9e87184df7af1df4
abadfish <kirsten.ofarrell@gmail.com>
2016-03-21 21:13:02 -0400

    clean

--- lib/matador/location.rb
+++ lib/matador/location.rb
@@ -1,10 +1,9 @@
 require_relative '../matador'
 require_relative 'cli'
 
+
 class Matador::Location
   attr_accessor :lattitude, :longitude, :location_1, :address, :input
-
-
 
   def self.here(address)
     location_1 = self.new
@@ -12,9 +11,5 @@ class Matador::Location
     location_1.longitude = Geocoder.coordinates(address)[1]
     location_1
   end
-
-  # def self.geosearch(input)
-  #   Geocoder.coordinates(input)
-  # end
 
 end
