d730609b1bb399542570273f522a9fc2139b0357
abadfish <kirsten.ofarrell@gmail.com>
2016-03-27 13:56:17 -0400

    improvements from review

--- lib/matadore/location.rb
+++ lib/matadore/location.rb
@@ -6,9 +6,10 @@ class Matadore::Location
   attr_accessor :lattitude, :longitude, :location_1, :address, :input
 
   def self.here(address)
-    location_1 = self.new
-    location_1.lattitude = Geocoder.coordinates(address)[0]
-    location_1.longitude = Geocoder.coordinates(address)[1]
-    location_1
+    self.new.tap do |location|
+    # location_array = Geocoder.coordinates(address)
+      location.lattitude, location.longitude = Geocoder.coordinates(address)
+    # location.longitude = location_array[1]
+    end
   end
 end
--- lib/matadore/scraper.rb
+++ lib/matadore/scraper.rb
@@ -6,13 +6,12 @@ class Matadore::Scraper
 
     doc = Nokogiri::HTML(open(location_url))
 
-    location = self.new
-    location.temp = doc.search("p.myforecast-current-lrg").text
-    location.pressure = doc.search("div#current_conditions_detail tr[3]    td").text.split("Barometer")[1]
-    location.humidity = doc.search("div#current_conditions_detail tr[1] td").text.split("Humidity")[1]
-    location.wind = doc.search("div#current_conditions_detail tr[2] td").text.split("Wind Speed")[1]
-    location.visibility = doc.search("div#current_conditions_detail tr[5] td").text.split("Visibility")[1]
-
-    location
+    self.new.tap do |location|
+      location.temp = doc.search("p.myforecast-current-lrg").text
+      location.pressure = doc.search("div#current_conditions_detail tr[3]    td").text.split("Barometer")[1]
+      location.humidity = doc.search("div#current_conditions_detail tr[1] td").text.split("Humidity")[1]
+      location.wind = doc.search("div#current_conditions_detail tr[2] td").text.split("Wind Speed")[1]
+      location.visibility = doc.search("div#current_conditions_detail tr[5] td").text.split("Visibility")[1]
+    end
   end
 end
